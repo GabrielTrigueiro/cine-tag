5bc615c72bfd262f7c3d8bbd9a5a0235b17e3319
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-07-01 17:50:23 -0300

    rodape

---
+++ src/components/Rodape/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import styled from "./Rodape.module.css";
+
+export default function Rodape() {
+  return (
+    <footer className={styled.rodape}>
+      <div>Desenvolvido por Alura.</div>
+    </footer>
+  );
+}

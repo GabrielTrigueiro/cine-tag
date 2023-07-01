10ac0239e3e4965d2bcd8d948064d15e6a943e13
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-07-01 16:22:07 -0300

    cabecalho e link

---
+++ src/components/Cabecalho/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import styles from "./Cabecalho.module.css";
+import logo from "imagens/Logo-cinetag-branco 1.png";
+import CabecalhoLink from "./CabecalhoLink";
+
+export default function Cabecalho() {
+  return (
+    <header className={styles.cabecalho}>
+      <Link to="./">
+        <img src={logo} alt="Logo do cinetag"></img>
+      </Link>
+      <nav>
+        <CabecalhoLink url="./">Home</CabecalhoLink>
+        <CabecalhoLink url="./Favoritos">Favoritos</CabecalhoLink>
+      </nav>
+    </header>
+  );
+}

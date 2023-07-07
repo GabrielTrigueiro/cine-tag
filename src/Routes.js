cd956a13ef596a629025fa1242bc166b8bf0ef7a
GabrielTrigueiro <gabrieltrigueirofernandes@gmail.com>
2023-07-07 10:20:08 -0300

    rota de favoritos

---
+++ src/Routes.js
@@ -0,0 +1,22 @@
+import Cabecalho from "components/Cabecalho";
+import Container from "components/Container";
+import Rodape from "components/Rodape";
+import Inicio from "pages/Inicio";
+import Favoritos from "pages/favoritos";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Cabecalho />
+      <Container>
+        <Routes>
+          <Route path="/" element={<Inicio />}></Route>
+          <Route path="/favoritos" element={<Favoritos />}></Route>
+        </Routes>
+      </Container>
+      <Rodape />
+    </BrowserRouter>
+  );
+}
